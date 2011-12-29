bd34846b8f650d1238935ceaf8ce6f496f06e60b
Vishveshwar Bhat <vishveshwar.bhat@ti.com>
2011-12-29 17:02:57 +0530

    tsl2550: modify to work as input driver
    The original version is a "misc" driver and user needed to
    read the light level from sysfs.
    
    To work well as Android sensor driver, this is modified to
    work as "input" driver. The light level is reported
    periodically as an input event from kernel.
    
    Modifications based on drivers/input/misc/gp2a.c
    from google android kernel
    
    Signed-off-by: Vishveshwar Bhat <vishveshwar.bhat@ti.com>
    Signed-off-by: satish <satish.patel@ti.com>

--- drivers/misc/tsl2550.c
+++ drivers/misc/tsl2550.c
@@ -24,6 +24,8 @@
 #include <linux/slab.h>
 #include <linux/i2c.h>
 #include <linux/mutex.h>
+#include <linux/input.h>
+#include <linux/wakelock.h>
 
 #define TSL2550_DRV_NAME	"tsl2550"
 #define DRIVER_VERSION		"1.2"
@@ -39,13 +41,29 @@
 #define TSL2550_READ_ADC0		0x43
 #define TSL2550_READ_ADC1		0x83
 
+/* start time delay for light sensor in nano seconds */
+#define LIGHT_SENSOR_START_TIME_DELAY 50000000
+
+#define BUFFER_NUM	6
+
+#define tsl2550_dbgmsg(str, args...) pr_debug("%s: " str, __func__, ##args)
+
 /*
  * Structs
  */
 
 struct tsl2550_data {
 	struct i2c_client *client;
+	struct input_dev *light_input_dev;
 	struct mutex update_lock;
+	struct workqueue_struct *wq;
+	struct wake_lock prx_wake_lock;
+	struct work_struct work_light;
+	struct hrtimer timer;
+	ktime_t light_poll_delay;
+	int lux_value_buf[BUFFER_NUM];
+	int index_count;
+	bool buf_initialized;
 
 	unsigned int power_state:1;
 	unsigned int operating_mode:1;
@@ -184,6 +202,27 @@ static int tsl2550_calculate_lux(u8 ch0, u8 ch1)
 	return lux > TSL2550_MAX_LUX ? TSL2550_MAX_LUX : lux;
 }
 
+static void tsl2550_light_enable(struct tsl2550_data *data)
+{
+	tsl2550_dbgmsg("starting poll timer, delay %lldns\n",
+		    ktime_to_ns(data->light_poll_delay));
+	/* push -1 to input subsystem to enable real value to go through next */
+	input_report_abs(data->light_input_dev, ABS_MISC, -1);
+	hrtimer_start(&data->timer, ktime_set(0, LIGHT_SENSOR_START_TIME_DELAY),
+					HRTIMER_MODE_REL);
+}
+
+static void tsl2550_light_disable(struct tsl2550_data *data)
+{
+	tsl2550_dbgmsg("cancelling poll timer\n");
+	hrtimer_cancel(&data->timer);
+	cancel_work_sync(&data->work_light);
+	/* mark the adc buff as not initialized
+	 * so that it will be filled again on next light sensor start
+	 */
+	data->buf_initialized = false;
+}
+
 /*
  * SysFS support
  */
@@ -199,26 +238,74 @@ static ssize_t tsl2550_show_power_state(struct device *dev,
 static ssize_t tsl2550_store_power_state(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
-	struct i2c_client *client = to_i2c_client(dev);
-	struct tsl2550_data *data = i2c_get_clientdata(client);
-	unsigned long val = simple_strtoul(buf, NULL, 10);
+	struct tsl2550_data *data = dev_get_drvdata(dev);
 	int ret;
+	bool new_value;
 
-	if (val < 0 || val > 1)
+	if (sysfs_streq(buf, "1"))
+		new_value = true;
+	else if (sysfs_streq(buf, "0"))
+		new_value = false;
+	else {
+		pr_err("%s: invalid value %d\n", __func__, *buf);
 		return -EINVAL;
+	}
 
 	mutex_lock(&data->update_lock);
-	ret = tsl2550_set_power_state(client, val);
+	ret = tsl2550_set_power_state(data->client, new_value);
 	mutex_unlock(&data->update_lock);
 
 	if (ret < 0)
 		return ret;
 
+	if (new_value)
+		tsl2550_light_enable(data);
+	else
+		tsl2550_light_disable(data);
+
 	return count;
 }
 
-static DEVICE_ATTR(power_state, S_IWUSR | S_IRUGO,
+static DEVICE_ATTR(enable, S_IWUSR | S_IRUGO | S_IWGRP,
 		   tsl2550_show_power_state, tsl2550_store_power_state);
+
+static ssize_t poll_delay_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	struct tsl2550_data *data = dev_get_drvdata(dev);
+	return sprintf(buf, "%lld\n", ktime_to_ns(data->light_poll_delay));
+}
+
+
+static ssize_t poll_delay_store(struct device *dev,
+				struct device_attribute *attr,
+				const char *buf, size_t size)
+{
+	struct tsl2550_data *data = dev_get_drvdata(dev);
+	int64_t new_delay;
+	int err;
+
+	err = strict_strtoll(buf, 10, &new_delay);
+	if (err < 0)
+		return err;
+
+	tsl2550_dbgmsg("new delay = %lldns, old delay = %lldns\n",
+		    new_delay, ktime_to_ns(data->light_poll_delay));
+	mutex_lock(&data->update_lock);
+	if (new_delay != ktime_to_ns(data->light_poll_delay)) {
+		data->light_poll_delay = ns_to_ktime(new_delay);
+		if (data->power_state) {
+			tsl2550_light_disable(data);
+			tsl2550_light_enable(data);
+		}
+	}
+	mutex_unlock(&data->update_lock);
+
+	return size;
+}
+
+static DEVICE_ATTR(poll_delay, S_IRUGO | S_IWUSR | S_IWGRP,
+		   poll_delay_show, poll_delay_store);
 
 static ssize_t tsl2550_show_operating_mode(struct device *dev,
 		struct device_attribute *attr, char *buf)
@@ -255,18 +342,20 @@ static ssize_t tsl2550_store_operating_mode(struct device *dev,
 static DEVICE_ATTR(operating_mode, S_IWUSR | S_IRUGO,
 		   tsl2550_show_operating_mode, tsl2550_store_operating_mode);
 
-static ssize_t __tsl2550_show_lux(struct i2c_client *client, char *buf)
+static int __tsl2550_show_lux(struct i2c_client *client)
 {
 	struct tsl2550_data *data = i2c_get_clientdata(client);
 	u8 ch0, ch1;
 	int ret;
 
 	ret = tsl2550_get_adc_value(client, TSL2550_READ_ADC0);
+
 	if (ret < 0)
 		return ret;
 	ch0 = ret;
 
 	ret = tsl2550_get_adc_value(client, TSL2550_READ_ADC1);
+
 	if (ret < 0)
 		return ret;
 	ch1 = ret;
@@ -278,40 +367,98 @@ static ssize_t __tsl2550_show_lux(struct i2c_client *client, char *buf)
 	if (data->operating_mode == 1)
 		ret *= 5;
 
-	return sprintf(buf, "%d\n", ret);
-}
-
-static ssize_t tsl2550_show_lux1_input(struct device *dev,
-			struct device_attribute *attr, char *buf)
-{
-	struct i2c_client *client = to_i2c_client(dev);
-	struct tsl2550_data *data = i2c_get_clientdata(client);
-	int ret;
-
-	/* No LUX data if not operational */
-	if (!data->power_state)
-		return -EBUSY;
-
-	mutex_lock(&data->update_lock);
-	ret = __tsl2550_show_lux(client, buf);
-	mutex_unlock(&data->update_lock);
-
 	return ret;
 }
 
-static DEVICE_ATTR(lux1_input, S_IRUGO,
-		   tsl2550_show_lux1_input, NULL);
-
 static struct attribute *tsl2550_attributes[] = {
-	&dev_attr_power_state.attr,
+	&dev_attr_enable.attr,
+	&dev_attr_poll_delay.attr,
 	&dev_attr_operating_mode.attr,
-	&dev_attr_lux1_input.attr,
 	NULL
 };
 
 static const struct attribute_group tsl2550_attr_group = {
 	.attrs = tsl2550_attributes,
 };
+
+static int lightsensor_get_luxvalue(struct tsl2550_data *data)
+{
+	int i = 0;
+	int j = 0;
+	unsigned int lux_total = 0;
+	int lux_avr_value;
+	unsigned int index = 0;
+	unsigned int lux_max = 0;
+	unsigned int lux_min = 0;
+	int value = 0;
+
+	/* get lux value */
+	mutex_lock(&data->update_lock);
+	value = __tsl2550_show_lux(data->client);
+	mutex_unlock(&data->update_lock);
+
+	if (value < 0) {
+		pr_err("lightsensor returned error %d\n", value);
+		return value;
+	}
+	tsl2550_dbgmsg("light value %d\n", value);
+
+	index = (data->index_count++) % BUFFER_NUM;
+
+	/* buffer initialize (light sensor off ---> light sensor on) */
+	if (!data->buf_initialized) {
+		data->buf_initialized = true;
+		for (j = 0; j < BUFFER_NUM; j++)
+			data->lux_value_buf[j] = value;
+	} else
+		data->lux_value_buf[index] = value;
+
+	lux_max = data->lux_value_buf[0];
+	lux_min = data->lux_value_buf[0];
+
+	for (i = 0; i < BUFFER_NUM; i++) {
+		lux_total += data->lux_value_buf[i];
+
+		if (lux_max < data->lux_value_buf[i])
+			lux_max = data->lux_value_buf[i];
+
+		if (lux_min > data->lux_value_buf[i])
+			lux_min = data->lux_value_buf[i];
+	}
+	lux_avr_value = (lux_total-(lux_max+lux_min))/(BUFFER_NUM-2);
+
+	if (data->index_count == BUFFER_NUM)
+		data->index_count = 0;
+
+	tsl2550_dbgmsg("average light value %d\n", lux_avr_value);
+	return lux_avr_value;
+}
+
+static void tsl2550_work_func_light(struct work_struct *work)
+{
+	struct tsl2550_data *data = container_of(work, struct tsl2550_data,
+					      work_light);
+
+	int adc = lightsensor_get_luxvalue(data);
+	if (adc >= 0) {
+		input_report_abs(data->light_input_dev, ABS_MISC, adc);
+		input_sync(data->light_input_dev);
+	}
+}
+
+/* This function is for light sensor.  It operates every a few seconds.
+ * It asks for work to be done on a thread because i2c needs a thread
+ * context (slow and blocking) and then reschedules the timer to run again.
+ */
+static enum hrtimer_restart tsl2550_timer_func(struct hrtimer *timer)
+{
+	struct tsl2550_data *data = container_of(timer, struct tsl2550_data, timer);
+	queue_work(data->wq, &data->work_light);
+	hrtimer_forward_now(&data->timer, data->light_poll_delay);
+	return HRTIMER_RESTART;
+}
+
+
 
 /*
  * Initialization function
@@ -352,7 +499,8 @@ static int __devinit tsl2550_probe(struct i2c_client *client,
 {
 	struct i2c_adapter *adapter = to_i2c_adapter(client->dev.parent);
 	struct tsl2550_data *data;
-	int *opmode, err = 0;
+	struct input_dev *input_dev;
+	int *opmode, err = -ENODEV;
 
 	if (!i2c_check_functionality(adapter, I2C_FUNC_SMBUS_WRITE_BYTE
 					    | I2C_FUNC_SMBUS_READ_BYTE_DATA)) {
@@ -363,7 +511,9 @@ static int __devinit tsl2550_probe(struct i2c_client *client,
 	data = kzalloc(sizeof(struct tsl2550_data), GFP_KERNEL);
 	if (!data) {
 		err = -ENOMEM;
-		goto exit;
+		pr_err("%s: failed to alloc memory for module data\n",
+		       __func__);
+		return -ENOMEM;
 	}
 	data->client = client;
 	i2c_set_clientdata(client, data);
@@ -383,22 +533,76 @@ static int __devinit tsl2550_probe(struct i2c_client *client,
 	dev_info(&client->dev, "%s operating mode\n",
 			data->operating_mode ? "extended" : "standard");
 
-	mutex_init(&data->update_lock);
-
 	/* Initialize the TSL2550 chip */
 	err = tsl2550_init_client(client);
 	if (err)
 		goto exit_kfree;
+
+	wake_lock_init(&data->prx_wake_lock, WAKE_LOCK_SUSPEND,
+		"prx_wake_lock");
+	mutex_init(&data->update_lock);
 
 	/* Register sysfs hooks */
 	err = sysfs_create_group(&client->dev.kobj, &tsl2550_attr_group);
 	if (err)
 		goto exit_kfree;
 
+	/* hrtimer settings.  we poll for light values using a timer. */
+	hrtimer_init(&data->timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	data->light_poll_delay = ns_to_ktime(200 * NSEC_PER_MSEC);
+	data->timer.function = tsl2550_timer_func;
+
+	/* the timer just fires off a work queue request.  we need a thread
+	 * to read the i2c (can be slow and blocking)
+	 */
+	data->wq = create_singlethread_workqueue("tsl2550_wq");
+	if (!data->wq) {
+		err = -ENOMEM;
+		pr_err("%s: could not create workqueue\n", __func__);
+		goto err_create_workqueue;
+	}
+	/* this is the thread function we run on the work queue */
+	INIT_WORK(&data->work_light, tsl2550_work_func_light);
+
+	/* allocate lightsensor-level input_device */
+	input_dev = input_allocate_device();
+	if (!input_dev) {
+		pr_err("%s: could not allocate input device\n", __func__);
+		err = -ENOMEM;
+		goto err_input_allocate_device_light;
+	}
+	input_set_drvdata(input_dev, data);
+	input_dev->name = "lightsensor-level";
+	input_set_capability(input_dev, EV_ABS, ABS_MISC);
+	input_set_abs_params(input_dev, ABS_MISC, 0, 1, 0, 0);
+
+	dev_info(&client->dev, "registering lightsensor-level input device\n");
+	err = input_register_device(input_dev);
+	if (err < 0) {
+		pr_err("%s: could not register input device\n", __func__);
+		input_free_device(input_dev);
+		goto err_input_register_device_light;
+	}
+	data->light_input_dev = input_dev;
+	err = sysfs_create_group(&input_dev->dev.kobj,
+				 &tsl2550_attr_group);
+	if (err) {
+		pr_err("%s: could not create sysfs group\n", __func__);
+		goto err_sysfs_create_group_light;
+	}
+
 	dev_info(&client->dev, "support ver. %s enabled\n", DRIVER_VERSION);
 
 	return 0;
-
+	/* error, unwind it all */
+err_sysfs_create_group_light:
+	input_unregister_device(data->light_input_dev);
+err_input_register_device_light:
+err_input_allocate_device_light:
+	destroy_workqueue(data->wq);
+err_create_workqueue:
+	mutex_destroy(&data->update_lock);
+	wake_lock_destroy(&data->prx_wake_lock);
 exit_kfree:
 	kfree(data);
 exit:
@@ -407,10 +611,18 @@ exit:
 
 static int __devexit tsl2550_remove(struct i2c_client *client)
 {
-	sysfs_remove_group(&client->dev.kobj, &tsl2550_attr_group);
+	struct tsl2550_data *data = i2c_get_clientdata(client);
+	sysfs_remove_group(&data->light_input_dev->dev.kobj,
+			   &tsl2550_attr_group);
+
+	destroy_workqueue(data->wq);
+	input_unregister_device(data->light_input_dev);
 
 	/* Power down the device */
 	tsl2550_set_power_state(client, 0);
+
+	mutex_destroy(&data->update_lock);
+	wake_lock_destroy(&data->prx_wake_lock);
 
 	kfree(i2c_get_clientdata(client));
 
