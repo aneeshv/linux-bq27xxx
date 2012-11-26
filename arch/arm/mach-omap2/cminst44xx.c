b24cc0df7714b4437b3b7ef6ba9da2d6d1647397
Hebbar, Gururaja <gururaja.hebbar@ti.com>
2012-11-26 12:35:51 +0530

    ARM: OMAP2: hwmod: Fix "register offset NULL check" bug
    omap4_cminst_wait_module_ready() checks for register offset not to be
    NULL.
    
    int omap4_cminst_wait_module_ready(u8 part, u16 inst, s16 cdoffs,
    				   u16 clkctrl_offs)
    {
    	int i = 0;
    
    	if (!clkctrl_offs)
    		return 0;
    
    However, for Modules inconsistent with MMR (Memory Mapped Register)
    style , this check causes an issue.
    As in case of AM33xx, not all _CM domains start with CLKSTCTRL at 0x00,
    like RTC module)
    
    Due to above check, no further check is done to actually verify if the
    module is ready. If such Module is running at slow clock (say at 32 KHz),
    then such modules are not actually checked for its readiness.
    Down the sequence, this return 0 is assumed as module to be ready which
    is wrong.
    
    Since the actual base is verified in omap4_cminst_read_inst_reg(), this
    check here is not required at all and hence can be removed.
    
    Signed-off-by: Hebbar, Gururaja <gururaja.hebbar@ti.com>

--- arch/arm/mach-omap2/cminst44xx.c
+++ arch/arm/mach-omap2/cminst44xx.c
@@ -284,9 +284,6 @@ int omap4_cminst_wait_module_ready(u8 part, u16 inst, s16 cdoffs,
 {
 	int i = 0;
 
-	if (!clkctrl_offs)
-		return 0;
-
 	omap_test_timeout(_is_module_ready(part, inst, cdoffs, clkctrl_offs),
 			  MAX_MODULE_READY_TIME, i);
 
