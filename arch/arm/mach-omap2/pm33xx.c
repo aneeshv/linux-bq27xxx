186b3cd3fecd261ed8d984bf1e591e1034f1cb53
ShankarMurthy, Akshay <akshay.s@ti.com>
2013-03-14 21:02:14 +0530

    ARM: OMAP: AM33XX: PM: Enable RTC wakeup for standby
    Add support for rtc wakeup from standby by keeping RTC module
    enabled during standby.
    
    RTC wakeup is corrected in the PG2.0 and hence it is supported
    only on PG2.x boards.
    
    To test RTC wakeup use below command:
     @ rtcwake -d /dev/rtc0 -m standby -s 5
    
    Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>

--- arch/arm/mach-omap2/pm33xx.c
+++ arch/arm/mach-omap2/pm33xx.c
@@ -102,11 +102,12 @@ static int am33xx_pm_suspend(void)
 {
 	int state, ret = 0;
 
-	struct omap_hwmod *gpmc_oh, *usb_oh, *gpio1_oh;
+	struct omap_hwmod *gpmc_oh, *usb_oh, *gpio1_oh, *rtc_oh;
 
 	usb_oh		= omap_hwmod_lookup("usb_otg_hs");
 	gpmc_oh		= omap_hwmod_lookup("gpmc");
 	gpio1_oh	= omap_hwmod_lookup("gpio1");	/* WKUP domain GPIO */
+	rtc_oh		= omap_hwmod_lookup("rtc");
 
 	omap_hwmod_enable(usb_oh);
 	omap_hwmod_enable(gpmc_oh);
@@ -121,6 +122,14 @@ static int am33xx_pm_suspend(void)
 		omap_hwmod_idle(usb_oh);
 
 	omap_hwmod_idle(gpmc_oh);
+
+	/*
+	 * Keep RTC module enabled during standby
+	 * for PG2.x to enable wakeup from RTC.
+	 */
+	if ((omap_rev() >= AM335X_REV_ES2_0) &&
+		(suspend_state == PM_SUSPEND_STANDBY))
+		omap_hwmod_enable(rtc_oh);
 
 	/*
 	 * Disable the GPIO module. This ensure that
@@ -200,6 +209,14 @@ static int am33xx_pm_suspend(void)
 	 */
 	if (suspend_state == PM_SUSPEND_STANDBY)
 		omap_hwmod_idle(usb_oh);
+
+	/*
+	 * Put RTC module to idle on resume from standby
+	 * for PG2.x.
+	 */
+	if ((omap_rev() >= AM335X_REV_ES2_0) &&
+		(suspend_state == PM_SUSPEND_STANDBY))
+		omap_hwmod_idle(rtc_oh);
 
 	ret = am33xx_verify_lp_state(ret);
 
