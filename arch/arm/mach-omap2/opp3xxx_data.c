8994f0af77bc0ce7cc28689169444e5b8fd72e62
Arun Joseph <arunjoseph@ti.com>
2013-06-06 16:44:44 +0530

    ARM: OMAP: AM33XX: Raise OPPNitro MPU voltage for PG2.0
    To support 1GHz, the MPu voltage must be 1.35v for PG2.0
    as well.
    
    Change-Id: Idf61865119dc414bf59a87fd29ff73601b85ed87
    Signed-off-by: Arun Joseph <arunjoseph@ti.com>

--- arch/arm/mach-omap2/opp3xxx_data.c
+++ arch/arm/mach-omap2/opp3xxx_data.c
@@ -182,7 +182,7 @@ static struct omap_opp_def __initdata am33xx_es1_0_opp_def_list[] = {
 #define AM33XX_ES2_0_VDD_MPU_OPP50_UV		950000
 #define AM33XX_ES2_0_VDD_MPU_OPP100_UV		1100000
 #define AM33XX_ES2_0_VDD_MPU_OPPTURBO_UV	1260000
-#define AM33XX_ES2_0_VDD_MPU_OPPNITRO_UV	1320000
+#define AM33XX_ES2_0_VDD_MPU_OPPNITRO_UV	1350000
 
 static struct omap_opp_def __initdata am33xx_es2_0_opp_def_list[] = {
 	/* MPU OPP1 - OPP50 */
